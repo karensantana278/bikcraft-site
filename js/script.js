2ed93a097d3acc9c0457c2f06fc5011d79c8ff5b
Myllena Lucena <karensantana278@gmail.com>
2022-02-12 23:06:14 -0300

    script para levar os dados selecionados para pag de orcamento

--- js/script.js
+++ js/script.js
@@ -11,3 +11,18 @@ function ativarLink(link){
 }
 
 links.forEach(ativarLink)
+
+
+//Script para levar os dados clicados na pagina de seguros e/ou bicicletas para pagina de orçamento.
+//1 - parte alterar o href do html do botao para inserir uma query "?":
+//?tipo=seguro&produto=prata no href do botao, obs.. tipo=seguro e produto=prata, são respectivamente o name e o value utilizados no radio buttom do html da pagina orcamento, então precisamos apenas atualizar os botoes da pagina de seguros e bicicletas para colocar a query string dessa forma, com base no que foi passado na pagina de orçamento.
+
+const parametros = new URLSearchParams(location.search)
+parametros.forEach(parametro => {
+    const elemento = document.getElementById(parametro)
+    if (elemento) {
+        elemento.checked = true
+    }
+})
+
+//script para clicar e abrir as perguntas frequentes, deixando apenas a primeira aberta.
