8884e33ca393be08a147b9cd0156719f34f3e681
Myllena Lucena <karensantana278@gmail.com>
2022-02-12 22:40:48 -0300

    adicionando menu ativo com js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Script para menu principal ativo
+const links = document.querySelectorAll('.header-menu a')
+
+
+function ativarLink(link){
+    let url = location.href
+    let href = link.href
+    if(url.includes(href)){
+        link.classList.add('ativo')
+    }
+}
+
+links.forEach(ativarLink)
